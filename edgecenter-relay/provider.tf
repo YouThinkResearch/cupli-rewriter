cbd5a706bf26c1325881cf0a7fbd8763490c4e1f
blits <andrew@youthink.io>
2025-07-04 14:30:39 +0400

    ref: add backend for terraform and workflow for syncing state

--- edgecenter-relay/provider.tf
+++ edgecenter-relay/provider.tf
@@ -2,6 +2,18 @@ terraform {
   required_version = ">= 0.13.0"
 
   # Remote state backend – Google Cloud Storage
+  backend "s3" {
+    bucket = "terraform-rewriter-state"
+    key    = "relay/terraform.tfstate"
+    region = "auto"
+
+    # R2 S3-compatible settings
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+
+    endpoint = "https://5cbd25b113ce377352027b6e84867d15.r2.cloudflarestorage.com"
+  }
   # backend "gcs" {
   #   # TODO: change this to an existing bucket in your GCP project
   #   bucket = "hetzner-metal-k8s-terraform-state"
---
+++ edgecenter-relay/terraform.tfvars
@@ -0,0 +1,18 @@
+
+
+account_id = "5cbd25b113ce377352027b6e84867d15"
+zone_id    = "b4dc00bd73c7b0de523c3fbd2e1ccd1d"
+proxy_host = "fwd.youthink.dev"
+
+# List of hosts to rewrite
+rewritten_hosts = [
+  "go.cup.li",
+  "survey.alchemer.com",
+  "www.surveygizmo.com",
+  "surveygizmolibrary.s3.amazonaws.com",
+  "d3hz8hujpo34t2.cloudfront.net"
+]
+
+# Certificate configuration
+cert_common_name = "*.fwd.youthink.dev"
+cert_sans        = []
